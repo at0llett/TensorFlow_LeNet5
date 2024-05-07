709f3a5a1bc7a0cad5ad2fa4f7b43de548ac4885
atollett <a.t.tollett@gmail.com>
2024-05-07 14:28:52 -0700

    added tensorflow and LeNet5

---
+++ main.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+# Load the MNIST dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize pixel values to be between 0 and 1
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Define the LeNet-5 architecture
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(6, (5, 5), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(16, (5, 5), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(120, activation='relu'),
+    tf.keras.layers.Dense(84, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+# Reshape the input data to match the LeNet-5 input shape
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+# Compile and train the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate the model
+model.evaluate(x_test, y_test, verbose=2)
